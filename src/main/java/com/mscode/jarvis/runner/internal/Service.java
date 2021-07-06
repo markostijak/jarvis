1650000b98a60f2450d9327acbe6ccf89ee517f4
Marko Stijak <markostijak@users.noreply.github.com>
2021-07-06 02:24:01 +0200

    Improvements

--- src/main/java/com/mscode/jarvis/runner/internal/Service.java
+++ src/main/java/com/mscode/jarvis/runner/internal/Service.java
@@ -8,6 +8,8 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.Waitable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.core.annotation.MergedAnnotation;
 
 import java.util.List;
@@ -15,6 +17,8 @@ import java.util.Optional;
 
 @Data
 @AllArgsConstructor
+@ToString(of = "name")
+@EqualsAndHashCode(of = "name")
 public class Service {
 
     private final String name;
