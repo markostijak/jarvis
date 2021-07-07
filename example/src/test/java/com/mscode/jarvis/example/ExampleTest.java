fe6c34c82f108d74709307c7757c1c347d316172
Marko Stijak <markostijak@users.noreply.github.com>
2021-07-07 03:12:56 +0200

    Improvements

--- example/src/test/java/com/mscode/jarvis/example/ExampleTest.java
+++ example/src/test/java/com/mscode/jarvis/example/ExampleTest.java
@@ -5,12 +5,25 @@ import com.mscode.jarvis.deployment.mysql.DeployMySql;
 import com.mscode.jarvis.deployment.redis.DeployRedis;
 import com.mscode.jarvis.engine.annotation.JarvisTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @JarvisTest
 @DeployMySql
 @DeployRedis
 @DeployKafka
 public class ExampleTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private KafkaTemplate<?, ?> kafkaTemplate;
 
     @Test
     public void test() {
